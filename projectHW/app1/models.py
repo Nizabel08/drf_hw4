c3ff0c7b63e91520373fd4c26242ad6684614031
Nizabel08 <Nizabel2019>
2025-08-30 17:53:48 +0400

    add app1 models

---
+++ projectHW/app1/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Teacher(models.Model) :
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Students(models.Model) :
+    name = models.CharField(max_length=100)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
